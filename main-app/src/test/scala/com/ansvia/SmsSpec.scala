7612772b13b6ea3be2466e4ffd21656604dabbcc
robin <robin@digaku.com>
2013-05-04 21:53:47 +0700

    add SmsSpec.

---
+++ main-app/src/test/scala/com/ansvia/SmsSpec.scala
@@ -0,0 +1,18 @@
+package com.ansvia
+
+import org.specs2.Specification
+
+/**
+ * Author: robin
+ * Date: 5/4/13
+ * Time: 9:53 PM
+ *
+ */
+class SmsSpec extends Specification {
+
+  def is =
+    sequential ^
+    p ^
+      "Sms should" ^
+    end
+}
